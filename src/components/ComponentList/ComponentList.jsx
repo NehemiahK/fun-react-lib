27f9d017a242e64fdb7a0a9cba68de7742f19e02
Ramki A <kvraamkeydev@gmail.com>
2020-06-28 04:07:25 +0530

    load components dynamically from components folder

--- src/components/ComponentList/ComponentList.jsx
+++ src/components/ComponentList/ComponentList.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import './componentlist.css'
-
-/*Add your component name to the array */
-const components = ['Avatar', 'Simple Button', 'Heading']
+import * as library from "./../"
 
 const ComponentList = (props) => {
-    return (<div className='component-list'>
-        <h2>Components</h2>
-        {components.map(c => <button key={c} onClick={() => { props.setDemo(c) }}>{c}</button>)}
-    </div>)
+    return (
+        <section className='docs-nav'>
+            <div className="docs-nav-header">
+                <span>Components</span>
+                <input type="search" placeholder="Search Component" />
+            </div>
+            <div className="docs-nav-list">
+                {Object.keys(library).map(lib => <span key={lib} onClick={() => { props.setDemo(lib) }}>{lib}</span>)}
+            </div>
+        </section>
+    )
 }
 
 export default ComponentList
