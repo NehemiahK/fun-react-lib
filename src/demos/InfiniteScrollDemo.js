bd94f4ab578dd74f1ab6651f46f6ae80a466d7b9
ferueda <fe.rueda@outlook.com>
2020-06-28 00:19:37 -0400

    added the InfiniteScrollDemo component. Uses the useFetchUsers custom hook and returns the InfiniteScroll component with data from the randomusers API

--- src/demos/InfiniteScrollDemo.js
+++ src/demos/InfiniteScrollDemo.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import InfiniteScroll from '../components/InfiniteScroll/InfiniteScroll';
+import infiniteScrollPath from '../docs/infinite-scroll.md';
 
 const API_BASE_URL = 'https://randomuser.me/api';
 
@@ -50,14 +52,52 @@ const useFetchUsers = initialState => {
 		isError,
 	};
 };
+
 const InfiniteScrollDemo = () => {
 	const [markdown, setMarkdown] = useState('');
+
+	useEffect(() => {
+		fetch(infiniteScrollPath)
+			.then(res => res.text())
+			.then(text => {
+				setMarkdown(text);
+			});
+	});
+
+	const { data, setPage, hasMore, isLoading, isError } = useFetchUsers({
+		data: [],
+		page: 1,
+		hasMore: true,
+		isLoading: false,
+		isError: false,
+	});
 
 	return (
 		<React.Fragment>
 			<ReactMarkdown source={markdown} />
 			<div>
 				<h2>My User List</h2>
+				<InfiniteScroll
+					hasMore={hasMore}
+					isLoading={isLoading}
+					isError={isError}
+					triggerFunction={() => {
+						setPage(page => page + 1);
+					}}
+					loadingComponent={<p>loading...</p>}
+					errorComponent={<p>An error ocurred...</p>}
+				>
+					{data.map((d, i) => {
+						return (
+							<div key={i}>
+								<h3>{`${d.name.title}. ${d.name.first} ${d.name.last}`}</h3>
+								<p>
+									{d.cell} - {d.email}
+								</p>
+							</div>
+						);
+					})}
+				</InfiniteScroll>
 			</div>
 		</React.Fragment>
 	);
