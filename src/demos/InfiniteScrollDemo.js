7be6a5711821776e0ba7b5c4377e65831c422509
ferueda <fe.rueda@outlook.com>
2020-06-28 00:16:20 -0400

    getUsers function that fetches a list of users from the randomuser API.

--- src/demos/InfiniteScrollDemo.js
+++ src/demos/InfiniteScrollDemo.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 
+const API_BASE_URL = 'https://randomuser.me/api';
+
+const getUsers = async (currentPage, results) => {
+	const res = await fetch(`${API_BASE_URL}/?page=${currentPage}&results=${results}`);
+	const data = await res.json();
+	return data.results;
+};
+
 const InfiniteScrollDemo = () => {
 	const [markdown, setMarkdown] = useState('');
 
