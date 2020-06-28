55f0066c8d2108964d06f42d0f12ede8823d433b
Ramki A <kvraamkeydev@gmail.com>
2020-06-28 17:15:54 +0530

    [FIX] InfiniteScroll added into docs

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@
 export { default as Avatar } from "./Avatar/Avatar";
 export { default as Heading } from "./Heading/Heading";
 export { default as SimpleButton } from "./SimpleButton/SimpleButton";
+export { default as InfiniteScroll } from "./InfiniteScroll/InfiniteScroll";
