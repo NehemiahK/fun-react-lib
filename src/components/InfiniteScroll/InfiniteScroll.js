bf9080d0d2f38e325c3153cfa15e32f360385525
ferueda <fe.rueda@outlook.com>
2020-06-28 00:20:44 -0400

    InfiniteScroll component initial commit

---
+++ src/components/InfiniteScroll/InfiniteScroll.js
@@ -0,0 +1,18 @@
+import React, { useRef, useEffect } from 'react';
+
+const InfiniteScroll = ({
+	children,
+	hasMore, // Required: Specifies if there are more entities to load
+	isLoading, // Required: Specifies if the current state of the app is loading. If true and a loadingComponent was passed, it will be shown
+	isError, // Required: Specifies if an error ocurred. If true and an errorComponent was passed, it will be shown
+	triggerFunction, // Required: When the last element is visible, it will trigger the passed function.
+	loadingComponent = null,
+	errorComponent = null,
+}) => {
+	return (
+		<div>
+		</div>
+	);
+};
+
+export default InfiniteScroll;
