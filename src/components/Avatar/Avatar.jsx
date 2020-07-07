7e4c6c6705f4360af1bc02c61261ef9ceeeb8768
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-07-07 06:01:55 +0530

    fix(Avatar.jsx): linting errors

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -3,31 +3,25 @@ import PropTypes from 'prop-types';
 
 import './Avatar.scss';
 
-const Avatar = (props) => {
+const Avatar = ({ name, src, size, className }) => {
   return (
-    <div className={`avatar ${props.size} ${props.className}`}>
-      {props.src && (
-        <img
-          className={`avatar ${props.size}`}
-          alt={props.name}
-          src={props.src}
-        />
-      )}
-      {props.name && !props.src && (
-        <span className={`avatar ${props.size}`}>{props.name}</span>
-      )}
+    <div className={`avatar ${size} ${className}`}>
+      {src && <img className={`avatar ${size}`} alt={name} src={src} />}
+      {name && !src && <span className={`avatar ${size}`}>{name}</span>}
     </div>
   );
 };
 
 Avatar.propTypes = {
   name: PropTypes.string.isRequired,
-  src: PropTypes.string,
+  src: PropTypes.string.isRequired,
   size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  className: PropTypes.string,
 };
 
 Avatar.defaultProps = {
   size: 'md',
+  className: '',
 };
 
 export default Avatar;
