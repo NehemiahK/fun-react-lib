c027a879eddb7ee0bdf9f26a8b65c6145f6333b2
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-07-07 07:13:56 +0530

    fix(DemoDisplay): linting errors

--- src/demos/DemoDisplay.jsx
+++ src/demos/DemoDisplay.jsx
@@ -1,45 +1,29 @@
+/* eslint-disable no-console */
 import React from 'react';
-import './DemoDisplay.scss';
-/*add your demo in the import statement below */
-import {
-  SimpleButtonDemo,
-  HeadingDemo,
-  ProgressBarDemo,
-} from '../exports/exports';
+import PropTypes from 'prop-types';
 
-const switchTo = (componentName) => {
-  switch (componentName) {
-    case 'Simple Button':
-      return <SimpleButtonDemo />;
-    case 'Heading':
-      return <HeadingDemo />;
-    case 'Progress Bar':
-      return <ProgressBarDemo />;
-    default:
-      return '';
-  }
-};
+import './DemoDisplay.scss';
 
 const importView = (DemoComponentName) =>
   React.lazy(() =>
     import(`./${DemoComponentName}`).catch((e) =>
-      console.log('demo component not exported')
+      console.error('demo component not exported', e)
     )
   );
 
-const DemoDisplay = (props) => {
+const DemoDisplay = ({ demo }) => {
   const [views, setViews] = React.useState();
 
   React.useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const comp = props.demo ? props.demo : urlParams.get('c');
+    const comp = demo || urlParams.get('c');
     console.log(`${comp}Demo`);
     async function loadViews() {
       const View = await importView(`${comp}Demo`);
       setViews(<View />);
     }
     loadViews();
-  }, [props.demo]);
+  }, [demo]);
 
   return (
     <React.Suspense
@@ -52,6 +36,11 @@ const DemoDisplay = (props) => {
       <div className="docs-content markdown-body">{views}</div>
     </React.Suspense>
   );
+};
+
+DemoDisplay.propTypes = {
+  demo: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)])
+    .isRequired,
 };
 
 export default DemoDisplay;
