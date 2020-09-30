19408de28196307357bf0f123d823140bfb182a5
Daksh Kulshreshtha <akshd@outlook.com>
2020-10-01 00:04:40 +0530

    added card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,22 +4,18 @@ import PropTypes from 'prop-types';
 import './Card.scss';
 
 export default function Card({ title, body }) {
-
   return (
     <div className="card">
       <h2>{title}</h2>
-      <span className="separator"></span>
+      <span className="separator" />
       <p>{body}</p>
     </div>
   );
-};
+}
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired
+  body: PropTypes.string.isRequired,
 };
 
-Card.defaultProps = {
-  title: 'Title',
-  body: 'Body'
-};
+Card.defaultProps = {};
--- src/demos/CardDemo.jsx
+++ src/demos/CardDemo.jsx
@@ -4,26 +4,25 @@ import Card from '../components/Card/Card';
 import simpleButtonPath from '../docs/card.md';
 
 const HeadingDemo = () => {
-    const [markdown, setMarkdown] = useState('');
+  const [markdown, setMarkdown] = useState('');
 
-    useEffect(() => {
-        fetch(simpleButtonPath)
-            .then((response) => response.text())
-            .then((text) => {
-                setMarkdown(text);
-            });
-    });
+  useEffect(() => {
+    fetch(simpleButtonPath)
+      .then((response) => response.text())
+      .then((text) => {
+        setMarkdown(text);
+      });
+  });
 
-    return (
-        <>
-            <ReactMarkdown source={markdown} />
-            <Card
-                title="Heading"
-                body="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores et a repellendus magnam, illum aut quod molestiae. Ipsam mollitia vero, iure repellat deleniti ea, qui repellendus praesentium iste pariatur fugit!"
-            />
-        </>
-    );
+  return (
+    <>
+      <ReactMarkdown source={markdown} />
+      <Card
+        title="Heading"
+        body="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores et a repellendus magnam, illum aut quod molestiae. Ipsam mollitia vero, iure repellat deleniti ea, qui repellendus praesentium iste pariatur fugit!"
+      />
+    </>
+  );
 };
 
 export default HeadingDemo;
-
